e8e04a5e746138aa958dae9b570a46340683a89c
NCleverly <rytan2005@gmail.com>
2017-04-20 18:10:20 -0700

    Initial commit of the Prism base app. Awaiting Jani's mock up.

---
+++ CloudMovieCollection/CloudMovieCollection/CloudMovieCollection/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CloudMovieCollection.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
